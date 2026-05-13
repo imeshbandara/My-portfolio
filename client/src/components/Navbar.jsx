f398b01904139dc3c85a34d6657894c15ee33af3
Imesh Bandara <imeshnbandara826@gmail.com>
2026-05-13 11:33:11 +0530

    added cv to download

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -37,9 +37,15 @@ const Navbar = ()=>{
           <a href="#contact" onClick={handleScroll('contact')} className="text-slate-300 hover:text-cyan-400 transition">Contact</a>
         </div>
         
-        <button className="bg-cyan-600 hover:bg-cyan-500 text-white px-6 py-3 rounded-full font-medium text-lg transition">
-          Resume
-        </button>
+       <a 
+         href="/Imesh_Bandara_CV.pdf" 
+         target="_blank" 
+         rel="noopener noreferrer"
+         download="Imesh_Bandara_CV.pdf"
+         className="bg-cyan-600 hover:bg-cyan-500 text-white px-6 py-3 rounded-full font-medium text-lg transition inline-block text-center"
+>
+  Resume
+</a>
       </div>
     </nav>
   );
